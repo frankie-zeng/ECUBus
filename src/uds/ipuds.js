f32fb7d90ae52494c1343d65571779d0ccf78efe
Frankie Zeng <frankie.zeng@nxp.com>
2020-09-10 15:28:58 +0800

    feat(doip):fix tcp stick

--- src/uds/ipuds.js
+++ src/uds/ipuds.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-constant-condition */
 /* eslint-disable no-empty */
 /* eslint-disable no-undef */
 /* eslint-disable no-unused-vars */
@@ -12,6 +13,7 @@ const log = require('electron-log')
 class IPUDS extends UDS {
     constructor(win) {
         super(win)
+        this.lastData = Buffer.alloc(0)
         this.header = Buffer.from([VER, VER ^ 0xff, 0, 0, 0, 0, 0, 0])
         this.timeout = 2000
         this.sDelay = 100
@@ -20,10 +22,12 @@ class IPUDS extends UDS {
         this.clientTypeList = [0, 4, 6, 7, 0x4002, 0x4004, 0x8001, 0x8002, 0x8003]
         this.udpFd = dgram.createSocket('udp4')
         this.udpFd.on('message', (msg, rinfo) => {
-            var ret = this.parseData(msg)
-            if (ret.err === 0) {
-                if (ret.type === 4) {
-                    this.emit('doipDeviceFound', [ret.data, rinfo])
+            var rets = this.parseData(msg)
+            for (var i in rets) {
+                if (rets[i].err > 0) {
+                    if (rets[i].type === 4) {
+                        this.emit('doipDeviceFound', [rets[i].data, rinfo])
+                    }
                 }
             }
         })
@@ -104,86 +108,90 @@ class IPUDS extends UDS {
                     delete this.cMap[key]
                 })
                 item.fd.on('data', (msg) => {
-                    var ret = this.parseData(msg)
-                    if (ret.err === 0) {
-                        if ((item.receive) || (ret.type === 7)) {
-                            item.receive = false
-                            if (ret.type === 6) {
-                                if (item.active === false) {
-                                    clearTimeout(item.timer)
-                                    item.active = true
-                                    this.emit('doipTcpStatus', {
-                                        key: key,
-                                        err: 0,
-                                        msg: 'Active Success',
-                                        data: ret.data
-                                    }
-                                    )
-                                } else {
-                                    this.info("The router has actived")
-                                }
-                            } else if (ret.type === 7) {
-                                item.fd.write(this.writeAliveRes(parseInt(active.sa)))
-                            } else if (ret.type === 0x8001) {
-                                clearTimeout(item.timer)
-                                this.debug(sprintf("uds response:%s.", ret.data.payload.join(',')))
-                                try {
-                                    if ((ret.data.payload[0] == 0x7F) && (ret.data.payload[2] == 0X78)) {
-                                        item.receive = true;
-                                        this.delay()
+                    var rets = this.parseData(msg)
+                    for (var i in rets) {
+                        var ret = rets[i]
+                        if (ret.err >0) {
+                            if ((item.receive) || (ret.type === 7)) {
+                                item.receive = false
+                                if (ret.type === 6) {
+                                    if (item.active === false) {
+                                        clearTimeout(item.timer)
+                                        item.active = true
+                                        this.emit('doipTcpStatus', {
+                                            key: key,
+                                            err: 0,
+                                            msg: 'Active Success',
+                                            data: ret.data
+                                        }
+                                        )
                                     } else {
-                                        if (this.checkFunc(this.writeData, ret.data.payload)) {
-                                            if (item.timer.hasRef()) {
-                                                item.receive = true;
-                                                this.info(":User insert a new delay")
-                                            } else {
-                                                this.step()
-                                            }
+                                        this.info("The router has actived")
+                                    }
+                                } else if (ret.type === 7) {
+                                    item.fd.write(this.writeAliveRes(parseInt(active.sa)))
+                                } else if (ret.type === 0x8001) {
+                                    clearTimeout(item.timer)
+                                    this.debug(sprintf("uds response:%s.", ret.data.payload.join(',')))
+                                    try {
+                                        if ((ret.data.payload[0] == 0x7F) && (ret.data.payload[2] == 0X78)) {
+                                            item.receive = true;
+                                            this.delay()
                                         } else {
+                                            if (this.checkFunc(this.writeData, ret.data.payload)) {
+                                                if (item.timer.hasRef()) {
+                                                    item.receive = true;
+                                                    this.info(":User insert a new delay")
+                                                } else {
+                                                    this.step()
+                                                }
+                                            } else {
+                                                this.emit('udsError', {
+                                                    msg: "User defined function return false",
+                                                    index: this.tableIndex
+                                                })
+                                            }
+                                        }
+                                    } catch (error) {
+                                        this.emit('udsError', {
+                                            msg: sprintf("User defined function syntax error,%s", error.message),
+                                            index: this.tableIndex
+                                        })
+                                    }
+
+                                } else if (ret.type === 0x8002) {
+                                    clearTimeout(item.timer)
+                                    this.debug(sprintf("ack:0x%X,msg:%s.\r\n", ret.data.code, ret.data.payload.join(',')))
+                                    if (item.suppress) {
+                                        setTimeout(() => {
+                                            this.step()
+                                        }, this.sDelay)
+                                    } else {
+                                        item.receive = true
+                                        item.timer = setTimeout(() => {
                                             this.emit('udsError', {
-                                                msg: "User defined function return false",
+                                                msg: 'No uds response',
                                                 index: this.tableIndex
                                             })
-                                        }
+                                        }, this.timeout)
                                     }
-                                } catch (error) {
+                                } else if (ret.type === 0x8003) {
+                                    clearTimeout(item.timer)
                                     this.emit('udsError', {
-                                        msg: sprintf("User defined function syntax error,%s", error.message),
+                                        msg: sprintf("nack:0x%X,msg:0x%s", ret.data.code, ret.data.payload),
                                         index: this.tableIndex
                                     })
                                 }
-
-                            } else if (ret.type === 0x8002) {
-                                clearTimeout(item.timer)
-                                this.debug(sprintf("ack:0x%X,msg:%s.\r\n", ret.data.code, ret.data.payload.join(',')))
-                                if (item.suppress) {
-                                    setTimeout(() => {
-                                        this.step()
-                                    }, this.sDelay)
-                                } else {
-                                    item.receive = true
-                                    item.timer = setTimeout(() => {
-                                        this.emit('udsError', {
-                                            msg: 'No uds response',
-                                            index: this.tableIndex
-                                        })
-                                    }, this.timeout)
-                                }
-                            } else if (ret.type === 0x8003) {
-                                clearTimeout(item.timer)
-                                this.emit('udsError', {
-                                    msg: sprintf("nack:0x%X,msg:0x%s", ret.data.code, ret.data.payload),
-                                    index: this.tableIndex
-                                })
                             }
+                        } else if (ret.err < 0) {
+                            clearTimeout(item.timer)
+                            this.emit('udsError', {
+                                msg: sprintf("%s", ret.msg),
+                                index: this.tableIndex
+                            })
                         }
-                    } else if (ret.err < 0) {
-                        clearTimeout(item.timer)
-                        this.emit('udsError', {
-                            msg: sprintf("%s", ret.msg),
-                            index: this.tableIndex
-                        })
                     }
+
 
                 })
             }
@@ -262,84 +270,104 @@ class IPUDS extends UDS {
         return 1
     }
     parseData(msg) {
-        /*header handler*/
-        var data = Buffer.from(msg)
-        var ret = {}
-        ret.err = 0
-        ret.msg = 'empty'
-        if ((data.readUInt8(0) !== VER) || data.readUInt8(1) !== (VER ^ 0xff)) {
-            ret.err = -1
-            ret.msg = "Incorrect pattern format"
-            return ret
-        }
-        var type = data.readUInt16BE(2)
-        if (this.typeList.indexOf(type) === -1) {
-            ret.err = -1
-            ret.msg = "Unknown payload type"
-            return ret
-        }
-        var len = data.readUInt32BE(4)
-        if (len !== (data.length - 8)) {
-            ret.err = -1
-            ret.msg = "Invalid payload length"
-            return ret
-        }
-        if (this.clientTypeList.indexOf(type) === -1) {
-            ret.err = -2
-            ret.msg = "Discard message"
-            return ret
-        }
-        /*real parse*/
-        var payload = Buffer.from(data.slice(8))
-        ret.type = type
-        ret.data = {}
-        if (type === 4 && payload.length >= 32) {
-            ret.data.vin = payload.slice(0, 17).toString('ascii')
-            ret.data.logicalAddr = payload.readUInt16BE(17)
-            ret.data.eid = payload.slice(19, 25).toString('hex')
-            ret.data.gid = payload.slice(25, 31).toString('hex')
-            ret.data.fAction = payload.readUInt8(31)
-            if (payload.length == 33) {
-                ret.data.syncStatus = payload.readUInt8(32)
+        var data = Buffer.concat([this.lastData, msg])
+        this.lastData = Buffer.alloc(0)
+        this.debug(msg.toString('hex'))
+        var rets = []
+        var payload
+        var header
+
+
+
+        while (1) {
+            let ret = {}
+            ret.err = 0
+            ret.msg = 'empty'
+            rets.push(ret)
+            if (data.length >= 8) {
+                header = Buffer.from(data.slice(0, 8))
+                /*header handler*/
+                if ((header.readUInt8(0) !== VER) || header.readUInt8(1) !== (VER ^ 0xff)) {
+                    ret.err = -1
+                    ret.msg = "Incorrect pattern format"
+                    return rets
+                }
+                let type = header.readUInt16BE(2)
+                if (this.typeList.indexOf(type) === -1) {
+                    ret.err = -1
+                    ret.msg = "Unknown payload type"
+                    return rets
+                }
+                let len = header.readUInt32BE(4)
+                if (this.clientTypeList.indexOf(type) === -1) {
+                    ret.err = -2
+                    ret.msg = "Discard message"
+                    return rets
+                }
+                if (data.length - 8 >= len) {
+                    /*a ok frame*/
+                    payload = Buffer.from(data.slice(8, 8 + len))
+                    ret.type = type
+                    ret.data = {}
+                    if (type === 4 && payload.length >= 32) {
+                        ret.data.vin = payload.slice(0, 17).toString('ascii')
+                        ret.data.logicalAddr = payload.readUInt16BE(17)
+                        ret.data.eid = payload.slice(19, 25).toString('hex')
+                        ret.data.gid = payload.slice(25, 31).toString('hex')
+                        ret.data.fAction = payload.readUInt8(31)
+                        if (payload.length == 33) {
+                            ret.data.syncStatus = payload.readUInt8(32)
+                        }
+                    } else if (type === 6) {
+                        ret.data.testerAddr = payload.readUInt16BE(0)
+                        ret.data.entityAddr = payload.readUInt16BE(2)
+                        ret.data.code = payload.readUInt8(4)
+                        if (payload.length == 13) {
+                            ret.data.option = payload.readUInt32BE(9)
+                        }
+                    } else if (type === 0x8001) {
+                        ret.data.sa = payload.readUInt16BE(0)
+                        ret.data.ta = payload.readUInt16BE(2)
+                        ret.data.payload = [...payload.slice(4)]
+                    } else if (type === 0x8002) {
+                        ret.data.sa = payload.readUInt16BE(0)
+                        ret.data.ta = payload.readUInt16BE(2)
+                        ret.data.code = payload.readUInt8(4)
+                        ret.data.payload = [...payload.slice(5)]
+                    } else if (type === 0x8003) {
+                        // ret.err=-1
+                        // ret.msg='Negative diagnostic message'
+                        ret.data.sa = payload.readUInt16BE(0)
+                        ret.data.ta = payload.readUInt16BE(2)
+                        ret.data.code = payload.readUInt8(4)
+                        ret.data.payload = [...payload.slice(5)]
+                    }
+                    else if (type === 7) {
+                        //todo active request
+                    } else if (type === 0x4004) {
+                        ret.data.powerMode = payload.readUInt8(0)
+                    } else if (type === 0x4002) {
+                        ret.data.nt = payload.readUInt8(0)
+                        ret.data.mcts = payload.readUInt8(1)
+                        ret.data.ncts = payload.readUInt8(2)
+                        if (payload.length === 7) {
+                            ret.data.mds = payload.readUInt32BE(3)
+                        }
+                    }
+                    log.info(JSON.stringify(ret))
+                    ret.err = len + 8
+                    data = Buffer.from(data.slice(8 + len))
+                } else {
+                    /* stroe and handle later*/
+                    this.lastData = Buffer.from(data)
+                    break
+                }
+            } else {
+                this.lastData = Buffer.from(data)
+                break
             }
-        } else if (type === 6) {
-            ret.data.testerAddr = payload.readUInt16BE(0)
-            ret.data.entityAddr = payload.readUInt16BE(2)
-            ret.data.code = payload.readUInt8(4)
-            if (payload.length == 13) {
-                ret.data.option = payload.readUInt32BE(9)
-            }
-        } else if (type === 0x8001) {
-            ret.data.sa = payload.readUInt16BE(0)
-            ret.data.ta = payload.readUInt16BE(2)
-            ret.data.payload = [...payload.slice(4)]
-        } else if (type === 0x8002) {
-            ret.data.sa = payload.readUInt16BE(0)
-            ret.data.ta = payload.readUInt16BE(2)
-            ret.data.code = payload.readUInt8(4)
-            ret.data.payload = [...payload.slice(5)]
-        } else if (type === 0x8003) {
-            // ret.err=-1
-            // ret.msg='Negative diagnostic message'
-            ret.data.sa = payload.readUInt16BE(0)
-            ret.data.ta = payload.readUInt16BE(2)
-            ret.data.code = payload.readUInt8(4)
-            ret.data.payload = [...payload.slice(5)]
         }
-        else if (type === 7) {
-            //todo active request
-        } else if (type === 0x4004) {
-            ret.data.powerMode = payload.readUInt8(0)
-        } else if (type === 0x4002) {
-            ret.data.nt = payload.readUInt8(0)
-            ret.data.mcts = payload.readUInt8(1)
-            ret.data.ncts = payload.readUInt8(2)
-            if (payload.length === 7) {
-                ret.data.mds = payload.readUInt32BE(3)
-            }
-        }
-        log.info(JSON.stringify(ret))
-        return ret
+        return rets
     }
 
     changeLen(len) {
