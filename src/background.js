392f1a6d29e2d35919a98c181b19794caa3a264e
Frankie Zeng <frankie.zeng@nxp.com>
2021-04-04 11:28:21 +0800

    feat(electron):upgrade to 9.4.0

--- src/background.js
+++ src/background.js
@@ -5,13 +5,12 @@ import { app, protocol, BrowserWindow, dialog, Menu, ipcMain} from 'electron'
 // import CANUDS from './uds/canuds.js'
 
 import {
-  createProtocol,
-  installVueDevtools 
+  createProtocol, 
 } from 'vue-cli-plugin-electron-builder/lib'
 import { compile } from 'vue-template-compiler'
 import { mapState } from 'vuex'
 import { CancellationToken } from "electron-updater"
-
+import installExtension, { VUEJS_DEVTOOLS } from 'electron-devtools-installer'
 
 const log = require('electron-log');
 // enable ipc 
@@ -140,7 +139,7 @@ app.on('ready', async () => {
     // If you are not using Windows 10 dark mode, you may uncomment these lines
     // In addition, if the linked issue is closed, you can upgrade electron and uncomment these lines
     try {
-      await installVueDevtools()
+      await installExtension(VUEJS_DEVTOOLS)
     } catch (e) {
       console.error('Vue Devtools failed to install:', e.toString())
     }
